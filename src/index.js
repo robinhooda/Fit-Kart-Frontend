3ce054d255088ec448be7a9c0aadc5b8ac0cbc7b
robinhooda <robinhooda66@gmail.com>
2021-06-09 12:48:22 +0530

    Add. products context

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import { CartProvider } from './contexts/cart-context'
 import { AuthProvider } from './contexts/AuthContext'
-
+import { ProductsProvider } from './contexts/products-context'
 ReactDOM.render(
   <React.StrictMode>
     <AuthProvider>
-      <CartProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </CartProvider>
+      <ProductsProvider>
+        <CartProvider>
+          <Router>
+            <App />
+          </Router>
+        </CartProvider>
+      </ProductsProvider>
     </AuthProvider>
   </React.StrictMode>,
   document.getElementById('root')
