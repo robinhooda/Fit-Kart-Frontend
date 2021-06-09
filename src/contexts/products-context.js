67e538a2a7275f03f25ab4b2e0738936f9468484
robinhooda <robinhooda66@gmail.com>
2021-06-09 12:49:50 +0530

    Add. products context

---
+++ src/contexts/products-context.js
@@ -0,0 +1,24 @@
+import React, { createContext, useContext, useReducer } from 'react'
+import { productsReducer } from '../reducers/Products-reducer'
+
+const ProductsContext = createContext()
+
+const ProductsProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(productsReducer, {
+    products: [],
+    sortBy: 'none',
+    priceRange: 0,
+  })
+
+  return (
+    <ProductsContext.Provider value={{ state, dispatch }}>
+      {children}
+    </ProductsContext.Provider>
+  )
+}
+
+const useProducts = () => {
+  return useContext(ProductsContext)
+}
+
+export { ProductsProvider, useProducts }
