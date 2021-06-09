b8a33b136610057b326ec58b9de64135a60c1a23
robinhooda <robinhooda66@gmail.com>
2021-06-09 12:53:22 +0530

    Add. fetch products from api and disptach to products context

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,16 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Shop.css'
 import ProductCard from '../../components/ProductCard/ProductCard'
 import Aside from '../../components/Aside/Aside'
 import { useCart } from '../../contexts/cart-context'
+import Loader from '../../components/Loader/Loader'
+import axios from 'axios'
+import { ProductData } from '../../api-data'
+import { useProducts } from '../../contexts/products-context'
 
 const Shop = () => {
-  const { products } = useCart()
-  return (
+  // const { products } = useCart()
+  // console.log({ products })
+
+  const [product, setProduct] = useState([])
+  const {
+    dispatch: productDispatch,
+  } = useProducts()
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    console.log("shop ka useEffectchal rha hai")
+    ;(async function () {
+      try {
+
+        const response = await axios.get('http://localhost:3020/products')
+        console.log(response.data.products)
+        console.log("data aa gya")
+        setProduct(response.data.products)
+        productDispatch({ type: 'PRODUCTS', payload: response.data.products })
+        setLoading(false)
+      } catch (error) {
+        console.log(error)
+      }
+    })()
+  }, [])
+
+  return loading ? (
+    <Loader />
+  ) : (
     <div className='Shop'>
       <Aside />
       <div className='main'>
-        {products.map((product) => {
+        {product.map((product) => {
           return (
             <ProductCard
               id={product._id}
@@ -18,7 +48,7 @@ const Shop = () => {
               url={product.url}
               price={product.price}
               discount={product.discount}
-              isNew={product.isNew}
+              isNewStock={product.isNewStock}
             />
           )
         })}
