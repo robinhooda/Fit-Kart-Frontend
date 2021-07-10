0991e179010c16a2fe5149e547a61a6d9a12a551
robinhooda <robinhooda66@gmail.com>
2021-07-10 20:27:12 +0530

    Fix. wishlist page

--- src/pages/private/WishList/WishList.jsx
+++ src/pages/private/WishList/WishList.jsx
@@ -1,11 +1,64 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useCart } from '../../../contexts/cart-context'
 import WishListCard from '../../../components/WishListCard/WishListCard'
 import NoItemsFoundSVG from '../../../assets/images/empty.svg'
 import { Link } from 'react-router-dom'
 import './WishList.css'
+import axios from 'axios'
+import { useAuth } from '../../../contexts/AuthContext'
 const WishList = () => {
   const { state } = useCart()
+  const { auth } = useAuth()
+
+  useEffect(() => {
+    ;(async function () {
+      try {
+        console.log('wishlist post ho rhi hai')
+        const response = await axios.post(
+          'http://localhost:3020/wishlist',
+          state.wishlist,
+          {
+            headers: {
+              'auth-token': auth,
+            },
+          }
+        )
+        const wishlistArr = response.data.wishList.wishlistItems
+        console.log({ wishlistArr })
+      } catch (err) {
+        console.log('Error!!!', err)
+      }
+    })()
+  }, [auth, state])
+
+  useEffect(() => {
+    ;(async function () {
+      const apiCart = state.cart.map((item) => {
+        const { quantity, ...product } = item
+        return {
+          product,
+          quantity,
+        }
+      })
+      try {
+        console.log('cart post ho rhi hai')
+        const response = await axios.post(
+          'http://localhost:3020/cart',
+          apiCart,
+          {
+            headers: {
+              'auth-token': auth,
+            },
+          }
+        )
+        const cart = response.data
+        console.log({ cart })
+      } catch (err) {
+        console.log('Error!!!', err)
+      }
+    })()
+  }, [auth, state])
+
   return (
     <div className='main'>
       {state.wishlist.length === 0 && (
@@ -15,7 +68,7 @@ const WishList = () => {
             className='noItemsFoundImg'
             alt='NoItemsFoundSVG'
           />
-          <h2 className="mar-t-md">Opps! No items are wishListed</h2>
+          <h2 className='mar-t-md'>Opps! No items are wishListed</h2>
           <button className='primary-button large-button mar-t-lg'>
             <Link className='links' to='/shop'>
               Browse products
@@ -27,6 +80,7 @@ const WishList = () => {
       {state.wishlist.map((product) => {
         return (
           <WishListCard
+            key={product._id}
             id={product._id}
             name={product.name}
             url={product.url}
